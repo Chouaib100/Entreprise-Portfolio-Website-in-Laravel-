3e597b2e2a6d28aaf826dce99012cfd2c6902839
Chouaib <chouaibzouine3@gmail.com>
2026-02-03 12:35:49 +0100

    Change stille of Logout button

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -36,13 +36,9 @@
                          <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>    
-                        
+                    <button class="btn btn-primary" type="submit">Logout</button>
+                </form>
+
                         </a></li>
                     </ul>
                 </li>
