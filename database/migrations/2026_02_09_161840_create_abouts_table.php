bae4e3b4ee9d9db28115ab43db80cfa09a189a57
Chouaib <chouaibzouine3@gmail.com>
2026-02-09 17:32:57 +0100

    Create modele and megrate databasse for About Section administration

--- database/migrations/2026_02_09_161840_create_abouts_table.php
+++ database/migrations/2026_02_09_161840_create_abouts_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
+            $table->string('short_desc');
+            $table->string('title1');
+            $table->string('title2');
+            $table->string('title3');
+            $table->text('description');
+            $table->string('photo');
+            $table->string('title_skills');
+            $table->string('short_desc_skills');
+            $table->string('skill1');
+            $table->string('skill1_percentage');
+            $table->string('skill2');
+            $table->string('skill2_percentage');
+            $table->string('skill3');
+            $table->string('skill3_percentage');
+            $table->string('skill4');
+            $table->string('skill4_percentage');
+
+
+
             $table->timestamps();
         });
     }
