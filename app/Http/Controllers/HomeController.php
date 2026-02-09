19de62261322af15eb814eb3e74aada398b2c0eb
Chouaib <chouaibzouine3@gmail.com>
2026-02-09 23:01:33 +0100

    Create update_about methode on controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -76,4 +76,52 @@ class HomeController extends Controller
 
 
     }
+
+
+     public function update_about(Request $request, $id){
+
+        $about= About::find($id);
+
+
+       $about->short_desc = $request->short_desc;
+        $about->title1 = $request->title1;
+        $about->title2 = $request->title2;
+        $about->title3 = $request->title3;
+        $about->description = $request->description;
+
+        $about->title_skills= $request->title_skills;
+        $about->short_desc_skills= $request->short_desc_skills;
+
+        $about->skill1 = $request->skill1;
+        $about->skill1_percentage = $request->skill1_percentage;
+
+        $about->skill2 = $request->skill2;
+        $about->skill2_percentage = $request->skill2_percentage;
+
+        $about->skill3 = $request->skill3;
+        $about->skill3_percentage = $request->skill3_percentage;
+
+        $about->skill4 = $request->skill4;
+        $about->skill4_percentage = $request->skill4_percentage;
+
+
+        $image = $request->photo;
+
+        If($image){
+
+            unlink('photo_about/'.$about->photo);
+
+            $photoname = time().'.'.$image->getClientOriginalExtension();
+
+            $image->move('photo_about',$photoname);
+
+            $about->photo = $photoname;
+
+        }
+        
+        $about->save();
+
+        return redirect()->route('read_about');
+
+    }
 }
