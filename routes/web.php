44f3d56a2d522a9edb657cce28605db450043ef8
Chouaib <chouaibzouine3@gmail.com>
2026-02-10 13:55:31 +0100

    Create Service Controller methodes , and add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServiceController;
+use App\Models\Service;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,6 +20,17 @@ Route::post('/update_home/{id}', [HomeController::class, 'update_home'])->name('
 Route::get('/read_about', [HomeController::class, 'read_about'])->name('read_about');
 Route::get('/edit_about/{id}', [HomeController::class, 'edit_about'])->name('edit_about');
 Route::post('/update_about/{id}', [HomeController::class, 'update_about'])->name('update_about');
+
+
+// Admin Service
+
+Route::get('/read_service', [ServiceController::class, 'read_service'])->name('read_service');
+Route::get('/edit_service/{id}', [ServiceController::class, 'edit_service'])->name('edit_service');
+Route::post('/update_service/{id}', [ServiceController::class, 'update_service'])->name('update_service');
+Route::get('/delete_service/{id}', [ServiceController::class, 'delete_service'])->name('delete_about');
+Route::post('/create_service/{id}', [ServiceController::class, 'create_service'])->name('create_service');
+
+
 
 
 Route::get('/dashboard', function () {
