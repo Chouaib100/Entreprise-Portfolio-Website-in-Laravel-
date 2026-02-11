ea4434c2a9654bca45163aeb33ea395b130c1567
Chouaib <chouaibzouine3@gmail.com>
2026-02-11 13:39:42 +0100

    Create team blade

---
+++ resources/views/admin/team.blade.php
@@ -0,0 +1,52 @@
+@extends('backend.master')
+@section('main')
+<div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Team Page</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active"><a class="btn btn-primary" href="{{route('add_team')}}">Add Member</a></li>
+
+                        </ol>
+
+
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+
+
+                            </div>
+                            <div class="card-body">
+
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Job</th>
+                                    <th scope="col">Edit</th>
+                                    <th scope="col">Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                @foreach($teams as $team)
+                                    <tr>
+                                    <td><img height="100" width="100" src="\photo_team\{{$team->photo}}"></td>
+                                    <td>{{$team->name}}</td>
+                                    <td>{{$team->job}}</td>
+
+                                    <td><a class="btn btn-warning" href="{{route('edit_team',$team->id)}}">Edit</a></td>
+                                    <td><a class="btn btn-danger" href="{{route('delete_team',$team->id)}}">Delete</a></td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                                </table>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </main>
+@endsection
